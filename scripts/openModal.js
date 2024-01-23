e638dbb9a990ab7a9e56a735d4f74fab140f870b
Melon West <i.melonpub@gmail.com>
2024-01-23 19:01:41 +0600

    adding modal close func

--- scripts/openModal.js
+++ scripts/openModal.js
@@ -1,5 +1,22 @@
 addEventListener("DOMContentLoaded", function () {
-    document.getElementById('modal1').addEventListener("click", function () {
+    function openModal() {
         document.getElementById('modalWrapper').style.display="flex"
-    })
+    }
+    document.addEventListener('click', function(event) {
+        if (event.target === document.getElementById('modalWrapper')) {
+            document.getElementById('modalWrapper').style.display = 'none';
+        }
+    });
+    document.getElementById('closeModal').addEventListener("click", function (){document.getElementById('modalWrapper').style.display = 'none';})
+    document.getElementById('modal1').addEventListener("click", openModal)
+    document.getElementById('modal2').addEventListener("click", openModal)
+    document.getElementById('modal3').addEventListener("click", openModal)
+    document.getElementById('modal4').addEventListener("click", openModal)
+    document.getElementById('modal5').addEventListener("click", openModal)
+    document.getElementById('modal6').addEventListener("click", openModal)
+    document.getElementById('modal7').addEventListener("click", openModal)
+    document.getElementById('modal8').addEventListener("click", openModal)
+    document.getElementById('modal9').addEventListener("click", openModal)
+    document.getElementById('modal10').addEventListener("click", openModal)
+    document.getElementById('modal11').addEventListener("click", openModal)
 })
