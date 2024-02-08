70f7723a58ad8767bfe1a05193fdfdf2bdfa0ae3
sakekoke <sakekoke42@gmail.com>
2024-02-08 19:43:36 +0600

    email + bug fix + loader

--- scripts/openModal.js
+++ scripts/openModal.js
@@ -8,29 +8,22 @@ addEventListener("DOMContentLoaded", function () {
         }
     });
     document.getElementById('closeModal').addEventListener("click", function (){document.getElementById('modalWrapper').style.display = 'none';})
-    document.getElementById('modal1').addEventListener("click", openModal)
-    document.getElementById('modal2').addEventListener("click", openModal)
-    document.getElementById('modal3').addEventListener("click", openModal)
-    document.getElementById('modal4').addEventListener("click", openModal)
-    document.getElementById('modal5').addEventListener("click", openModal)
-    document.getElementById('modal6').addEventListener("click", openModal)
-    document.getElementById('modal7').addEventListener("click", openModal)
-    document.getElementById('modal8').addEventListener("click", openModal)
-    document.getElementById('modal9').addEventListener("click", openModal)
-    document.getElementById('modal10').addEventListener("click", openModal)
-    document.getElementById('modal11').addEventListener("click", openModal)
-    document.getElementById('modal21').addEventListener("click", openModal)
-    document.getElementById('modal22').addEventListener("click", openModal)
-    document.getElementById('modal23').addEventListener("click", openModal)
-    document.getElementById('modal24').addEventListener("click", openModal)
-    document.getElementById('modal25').addEventListener("click", openModal)
-    document.getElementById('modal26').addEventListener("click", openModal)
 
+    let buttonIds = ['modal1', 'modal2', 'modal3', 'modal4', 'modal5', 'modal6', 'modal7', 'modal8', 'modal9',
+        'modal10', 'modal11', 'modal21', 'modal22', 'modal23', 'modal24', 'modal25', 'modal26']
 
-    document.getElementById('openDrop').addEventListener("click", function (){
-        document.getElementById('dropDown').style.display='block'
-    })
-    document.getElementById('dropDown').addEventListener("click", function (){
-        document.getElementById('dropDown').style.display='none'
-    })
+    for (let i = 0; i < buttonIds.length; i++) {
+        if (document.getElementById(buttonIds[i])) {
+            document.getElementById(buttonIds[i]).addEventListener("click", openModal)
+        }
+    }
+
+    if (document.getElementById('openDrop') && document.getElementById('dropDown')) {
+        document.getElementById('openDrop').addEventListener("click", function (){
+            document.getElementById('dropDown').style.display='block'
+        })
+        document.getElementById('dropDown').addEventListener("click", function (){
+            document.getElementById('dropDown').style.display='none'
+        })
+    }
 })
